6525f9562cca63903a2ca72f4cf1f4a227efd1de
Dudeverse <se352@njit.edu>
2023-03-02 23:55:58 -0500

    fixed msp25

--- flask_sample/dsa/msp25.py
+++ flask_sample/dsa/msp25.py
@@ -1,14 +1,14 @@
 import random
 import time
-
+import math
 import sys
 sys.setrecursionlimit(10000)
 print(sys.getrecursionlimit())
 
 #tune n to higher numbers to see how the running time changes
 
-n = 100 # number of elements
-num_permutations = 1000 # number of permutations to generate
+n = 10 # number of elements
+num_permutations = 10 # number of permutations to generate
 
 # generate num_permutations random permutations
 permutations = []
@@ -25,39 +25,54 @@ print(len(permutations))
 print(type(permutations[0]))
 print(type(permutations[0][0]))
 
+import math
 
-def merge_sort_p25(arr):
+def merge_sort_k_2_5(arr):
     if len(arr) <= 1:
         return arr
-
-    # Determine the sizes of the two sublists
-    p = 2/5
+    
+    k = 2/5
     n = len(arr)
-    sublist_size_1 = int(p * n)
-    sublist_size_2 = n - sublist_size_1
+    subarray_size = math.ceil(n * k)
+    subarrays = [arr[i:i+subarray_size] for i in range(0, n, subarray_size)]
+    
+    sorted_subarrays = [merge_sort_k_2_5(subarray) for subarray in subarrays]
+    sorted_arr = merge_sorted_subarrays(sorted_subarrays)
+    
+    return sorted_arr
 
-    # Recursively sort the two sublists
-    sublist_1 = merge_sort_p25(arr[:sublist_size_1])
-    sublist_2 = merge_sort_p25(arr[sublist_size_1:])
+def merge_sorted_subarrays(sorted_subarrays):
+    if len(sorted_subarrays) == 1:
+        return sorted_subarrays[0]
+    
+    merged_subarrays = []
+    for i in range(0, len(sorted_subarrays), 2):
+        if i == len(sorted_subarrays) - 1:
+            merged_subarray = sorted_subarrays[i]
+        else:
+            merged_subarray = merge(sorted_subarrays[i], sorted_subarrays[i+1])
+        merged_subarrays.append(merged_subarray)
+    
+    return merge_sorted_subarrays(merged_subarrays)
 
-    # Merge the sorted sublists
-    merged_arr = []
-    i = 0
-    j = 0
-    while i < len(sublist_1) and j < len(sublist_2):
-        if sublist_1[i] < sublist_2[j]:
-            merged_arr.append(sublist_1[i])
+def merge(left, right):
+    result = []
+    i, j = 0, 0
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            result.append(left[i])
             i += 1
         else:
-            merged_arr.append(sublist_2[j])
+            result.append(right[j])
             j += 1
-    merged_arr += sublist_1[i:]
-    merged_arr += sublist_2[j:]
+    result += left[i:]
+    result += right[j:]
+    return result
 
-    return merged_arr
-
-
-
+# Test the code with the input array [2, 9, 0, 1, 4, 7, 8, 5]
+arr = [2, 9, 0, 1, 4, 7, 8, 5]
+sorted_arr = merge_sort_k_2_5(arr)
+print(sorted_arr)
 
 start = time.time()
 
@@ -66,7 +81,7 @@ for each_perm in permutations:
     print("---------count:", count)
     #print(each_perm)
     #print(len(each_perm))
-    sorted_arr = merge_sort_p25(each_perm)
+    sorted_arr = merge_sort_k_2_5(each_perm)
     #print("Sorted array is:", sorted_arr)
     #print(len(sorted_arr))
     count = count + 1
